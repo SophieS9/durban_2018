925e6d13f22c677ef19a38af2375f7babc4d1f0c
Ubuntu <ubuntu@ip-172-30-3-46.ec2.internal>
2018-10-02 15:11:55 +0200

    tidying up

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ working_dir=$(pwd)
 
 ## Ansible
 # Setup Ansible
-#sudo apt-get update # need to update on first run
-#sudo apt-get install -y git python-pip python-dev python-cffi libssl-dev debconf-utils libaio1 aptitude
-#sudo -H pip install --upgrade pip
-#sudo -H pip install ansible
+sudo apt-get update # need to update on first run
+sudo apt-get install -y git python-pip python-dev python-cffi libssl-dev debconf-utils libaio1 aptitude
+sudo -H pip install --upgrade pip
+sudo -H pip install ansible
 
 # as much as I love cowsay, lets turn it off so Ansible's messages aren't cows
 export ANSIBLE_NOCOWS=1
